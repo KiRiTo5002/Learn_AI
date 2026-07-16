a6b726e3af490256e87d07b501fd992fff074e3c
Pranjal Semalty <pranjalsemalty@gmail.com>
2026-07-16 12:03:58 +0530

    Added Day1 and Day2

---
+++ week1/day2/sys_temp.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+
+my_api_key = os.getenv("GROQ_API_KEY")
+if not my_api_key:
+    raise ValueError("GROQ_API_KEY environment variable is not set.")
+client = Groq(api_key=my_api_key)
+model = "llama-3.3-70b-versatile"
+role = "user"
+Prompt = "I Love You"
+# system
+message_system = [{"role": "system", "content": "You are my freeaky girlfriend."}]
+message = [{"role": role, "content": Prompt}]
+# temprature
+response = client.chat.completions.create(
+    model=model, messages=message_system + message, temperature=1
+)
+print("###########################################################")
+print(response.choices[0].message.content)
